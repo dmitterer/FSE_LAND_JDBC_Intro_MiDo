fdde0932374d9379ee9afdb9cc1cbe4fa3d35e84
Dominik <dmitterer@tsn.at>
2022-02-17 20:20:31 +0100

    1) Hinterlegen der Verbindung zur Datenbank inkl User und Passwort. Verbindung wurde erfolgreich hergestellt (siehe Bild in der Worddatei beim Unterpunkt 4.4 Verbindung zur Datenbank)

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -1,10 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcDemo {
 
     public static void main(String[] args) {
 
         System.out.println("JDBC Demo!");
+        sellectAllDemo();
 
     }
 
+    public static void sellectAllDemo(){
+
+
+        System.out.println("Select Demo mit Jdbc");
+
+        String sqlSelectAllPersons = "Select * from `student`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, pwd))
+
+        {
+            System.out.println("Verbindung zur DB hergestellt!");
+        } catch (SQLException e){
+
+            System.out.println("Fehler beim Aufbau der Verbindung zur Datenbank: " + e.getMessage());
+
+        }
+    }
 
 }
